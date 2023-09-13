de31dc387c075415e93be6dc0f2c7859fe8e104e
Adam46072 <adam.bartl2001@seznam.cz>
2023-09-13 15:12:34 +0200

    transport enterprise with route complete

--- TransportCompany/Route.cs
+++ TransportCompany/Route.cs
@@ -8,5 +8,17 @@ namespace TransportCompany
 {
     internal class Route
     {
+        public string nameRoute { get; private set; }
+        public Vehicle vehicle { get; private set; }
+        public List<Station> stations { get; private set; }
+
+        public Route(string nameRoute, Vehicle vehicle, Station[] stations)
+        {
+            this.nameRoute = nameRoute;
+            this.vehicle = vehicle;
+            this.stations = new List<Station>(stations);
+        }
+
+
     }
 }
--- TransportCompany/TransportEnterprise.cs
+++ TransportCompany/TransportEnterprise.cs
@@ -8,5 +8,18 @@ namespace TransportCompany
 {
     internal class TransportEnterprise
     {
+        public string NameTransportEnterprise { get; private set; }
+        public List<Route> routes { get; private set; }
+        public TransportEnterprise(string NameTransportEnterprise)
+        {
+            this.NameTransportEnterprise = NameTransportEnterprise;
+            routes = new List<Route>();
+        }
+        public void newRoute(string nameRoute, Vehicle vehicle, params Station[] stations)
+        {
+            routes.Add(new Route(nameRoute, vehicle, stations));
+        }
+
+
     }
 }
